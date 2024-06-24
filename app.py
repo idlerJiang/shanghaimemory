6962bd3df4050886754712dc0a39068ac705aa45
idlerJiang <935365680@qq.com>
2024-06-24 20:33:09 +0800

    图片补齐

--- app.py
+++ app.py
@@ -23,13 +23,45 @@ def quiz():
 def shmap():
     return render_template('shmap.html')
 
+@app.route('/comments')
+def comments():
+    return render_template('comments.html')
+
 @app.route('/timeline')
 def timeline():
     return render_template('timeline.html')
 
-@app.route('/ep2')
-def ep2():
+@app.route('/episode1')
+def episode1():
+    return render_template('episode1.html')
+
+@app.route('/episode2')
+def episode2():
     return render_template('episode2.html')
+
+@app.route('/episode3')
+def episode3():
+    return render_template('episode3.html')
+
+@app.route('/episode4')
+def episode4():
+    return render_template('episode4.html')
+
+@app.route('/episode5')
+def episode5():
+    return render_template('episode5.html')
+
+@app.route('/episode6')
+def episode6():
+    return render_template('episode6.html')
+
+@app.route('/episode7')
+def episode7():
+    return render_template('episode7.html')
+
+@app.route('/episode8')
+def episode8():
+    return render_template('episode8.html')
 
 if __name__ == '__main__':
     app.run()
